da0a6f254a2f9185a1cd606915ced5eec8e67ae8
1Dimitri <git.dimitri@janczak.net>
2020-07-28 10:22:47 +0200

    Create uninstall.cmd

---
+++ scripts/uninstall.cmd
@@ -0,0 +1,3 @@
+pushd %~dp0
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Installutil.exe /u  bin\release\InventoryWMIProvider.dll
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\gacutil.exe" /u InventoryWMIProvider
